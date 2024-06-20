08d95e78a1791739c7416471c7a6bf10af88a45a
amulya G <ammuammulya324@gmail.com>
2024-06-20 14:22:30 +0530

    Dmart class

---
+++ Dmart.java
@@ -0,0 +1,35 @@
+class Dmart{
+
+static String vegitables[]={"tamato","patato","cabbage","carrot","onipn","mushroom","pumpkin", "broccoli","green peas","brinjal"};
+static String fruits[]= {"apple","banana","orange","mango","grapes","Pineapple","pomegranate","avocado","coconut","papaya","watermelon","Strawberry","cherry","Apricot"};
+static String coolDrinks[]={"fanta","pepsi","limea","slice"};
+static String groceries[]={"annapurna","maggi","oil","colgate"};
+static String chocolates[]={"dairymilk","kitkat","mikybar","munch","park","snickers","safari"};
+static String cereals[]={"pea","groundnut","barley","sunseed","corn"};
+static String powders[]={"chille","garam","turmaric"};
+static String perfumes[]={"floral","citrus","fruity"};
+static String acnes[]={"illia beauty","tarte","bare"};
+static String lipsticks[]={"glossykiss","earthy","prettyinpink"};
+static String creams[]={"lakme","ponds","whitening"};
+static String snacks[]={"beef jerky","crackers","corn snacks","milkshakes"};
+static String bags[]={"handbags","potli bags","beach bags","laptop bags"};
+static String watchs[]={"fastrack","casio","smartwatch","sonata"};
+static String bracelets[]={"sterling silver","okosRose gold plated pink","silberry"};
+public static void main(String [] shop){
+	System.out.println(" the vegitables available are:"+ "\n" +vegitables[0]+"\n"+vegitables[1]+"\n"+vegitables[2]+"\n"+vegitables[3]+"\n"+vegitables[4]+"\n"+vegitables[5]+"\n"+vegitables[6]+"\n"+vegitables[7]+"\n"+vegitables[8]+"\n"+vegitables[9]);
+	System.out.println(" the fruits available are:"+ "\n" +fruits[0]+"\n"+fruits[1]+"\n"+fruits[2]+"\n"+fruits[3]+"\n"+fruits[4]+"\n"+fruits[5]+"\n"+fruits[6]+"\n"+fruits[7]+"\n"+fruits[8]+"\n"+fruits[9]+"\n"+fruits[10]+"\n"+fruits[11]+"\n"+fruits[12]+"\n"+fruits[13]);
+    System.out.println(" the coolDrinks available are:"+ "\n"+coolDrinks[0]+"\n"+coolDrinks[1]+"\n"+coolDrinks[2]+"\n"+coolDrinks[3]);
+	System.out.println(" the groceries available are:" +"\n" +groceries[0]+"\n"+groceries[1]+"\n"+groceries[2]+"\n"+groceries[3]);
+	System.out.println("the chocolates list here:" +"\n"+chocolates[0]+"\n "+chocolates[1]+" \n"+chocolates[2]+"\n "+chocolates[3]+" \n"+chocolates[4]+"\n "+chocolates[5]+"\n "+chocolates[6]);
+	System.out.println(" the groceries available are:"+ "\n"+cereals[0]+"\n"+cereals[1]+"\n"+cereals[2]+"\n"+cereals[3]+"\n"+cereals[4]);
+	System.out.println(" the powders available are:" +"\n"+powders[0]+"\n"+powders[1]+"\n"+powders[2]);
+	System.out.println(" the perfumes available are:" +"\n"+perfumes[0]+"\n"+perfumes[1]+"\n"+perfumes[2]);
+	System.out.println(" the acnes available are:"+"\n" +acnes[0]+"\n"+acnes[1]+"\n"+acnes[2]);//mackup
+	System.out.println(" the lipstick available are:"+ "\n"+lipsticks[0]+"\n"+lipsticks[1]+"\n"+lipsticks[2]);
+	System.out.println(" the creams available are:"+"\n" +creams[0]+"\n"+creams[1]+"\n"+creams[2]);
+	System.out.println(" the snacks available are:" +"\n" +snacks[0]+"\n"+snacks[1]+"\n"+snacks[2]+"\n"+snacks[3]);
+	System.out.println(" the bags available are:" +"\n"+bags[0]+"\n"+bags[1]+"\n"+bags[2]+"\n"+bags[3]);
+	System.out.println(" the watchs available are:"+"\n" +watchs[0]+"\n"+watchs[1]+"\n"+watchs[2]+"\n"+watchs[3]);
+	System.out.println(" the bracelets available are:"+"\n"+bracelets[0]+"\n"+bracelets[1]+"\n"+bracelets[2]);
+}
+}
