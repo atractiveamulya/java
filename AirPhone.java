263fcbab3d3e433f9e9918f3d30ca9236a899c8f
amulya G <ammuammulya324@gmail.com>
2024-06-21 14:22:41 +0530

    new file

---
+++ AirPhone.java
@@ -0,0 +1,14 @@
+class AirPhone {
+	public static void main(String [] AirPhone) {
+		String username = "AmulyaDalini";
+		int productID=8;
+		long moduleNumber = 8456778860L;
+		long contactNoForOrder = 8475627818L;
+		String price = "1000/-";
+		System.out.println("the user name is:"+username);
+		System.out.println(" the productID is:"+productID);
+		System.out.println("the moduleNumber is:"+moduleNumber);
+		System.out.println("the contactNoForOrder is:"+contactNoForOrder);
+		System.out.println("the price of the airphone is"+price);
+		}
+		}
